20bb7d25e28dcb2e30cc89c42b96a221e64c596a
Aya-Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2023-12-13 16:05:00 +0300

    JS Done

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ function fillButton(index, text) {
 }
 // pre-made a function. You can use this function to present an alert to say someone wins
 function winningAlert(winner) {
-  if(confirm(`Horraaay, ${winner} wins!`)){
-     // The code here will be exectued if you press on OK button that will pop on the window 
+  if (confirm(`Horraaay, ${winner} wins!`)) {
+    // The code here will be exectued if you press on OK button that will pop on the window 
   }
 }
 
 // SAMPLE CODE: This code fills the 1st and 9th button with X and O initially
 // ❗️ Delete this code once you are done testing
-fillButton(1, "X");
-fillButton(9, "O");
+// fillButton(1, "X");
+// fillButton(9, "O");
 
 /**
  *
@@ -24,11 +24,54 @@ fillButton(9, "O");
  * Add your code here, since this is going to be your main function
  * That interacts with the UI
  */
+
+let player = "X";
+let counter = 0;
 function clickButton(index) {
+  if (player == "X" && document.getElementById(index).innerHTML == '') {
+    fillButton(index, "X");
+    player = "O";
+    counter++;
+  } else if (player == "O" && document.getElementById(index).innerHTML == '') {
+    fillButton(index, "O");
+    player = "X";
+    counter++;
+  }
+  checkWinner()
   console.log(`Button number ${index} is clicked`);
+
   // Your main code here.
 }
 
+function checkWinner() {
+  winnerStream = [
+    [1, 2, 3],
+    [1, 4, 7],
+    [1, 5, 9],
+    [3, 6, 9],
+    [3, 5, 7],
+    [2, 5, 8],
+    [4, 5, 6],
+    [7, 8, 9]
+  ]
+  if (counter == 9) {
+    for (let i = 0; i < winnerStream.length; i++) {
+      block = winnerStream[i]
+      b1 = document.getElementById(block[0]).innerHTML
+      b2 = document.getElementById(block[1]).innerHTML
+      b3 = document.getElementById(block[2]).innerHTML
+      if (b1 == b2 && b1 == b3) {
+        winningAlert(b1);
+        break;
+      }
+
+    }
+  }
+}
+
+function restartGame() {
+  location.reload()
+}
 /**
  * (Optional) It's always a good idea to make a function for every single purpose.
  */
